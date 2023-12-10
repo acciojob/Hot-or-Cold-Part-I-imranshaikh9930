571dae2677e66b482784dc9633c8d04afb263d75
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-10 14:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 //your code here
 
+document.addEventListener('DOMContentLoaded', function () {
+    const responseElement = document.getElementById('respond');
+    const btn = document.querySelector("#btn");
+    btn.addEventListener("click",makeGuess);
+    let secretNumber = generateRandomNumber();
+
+    console.log(secretNumber)
+    let previousGuess;
+
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
+    }
+
+    function makeGuess() {
+        const guessInput = document.getElementById('guess');
+        const userGuess = parseInt(guessInput.value, 10);
+
+        if (isNaN(userGuess)) {
+            responseElement.textContent = 'Please enter a valid number.';
+            return;
+        }
+
+        if (previousGuess !== undefined) {
+            const currentDifference = Math.abs(secretNumber - userGuess);
+            const previousDifference = Math.abs(secretNumber - previousGuess);
+
+            if (currentDifference < previousDifference) {
+                responseElement.textContent = 'Getting hotter! ';
+                if (userGuess < secretNumber) {
+                    responseElement.textContent += 'Guess higher.';
+                } else {
+                    responseElement.textContent += 'Guess lower.';
+                }
+            } else {
+                responseElement.textContent = 'Getting colder. ';
+                if (userGuess < secretNumber) {
+                    responseElement.textContent += 'Guess higher.';
+                } else {
+                    responseElement.textContent += 'Guess lower.';
+                }
+            }
+        }
+
+        previousGuess = userGuess;
+    }
+});
